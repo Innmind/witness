f35d72a29aa309b826b8e9b29e1e77f1322ba56c
Baptiste Langlade <baptouuuu@gmail.com>
2024-11-16 14:57:59 +0100

    add null mailbox in order to create addresses that point to nothing

---
+++ src/Actor/Mailbox/Nil.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Witness\Actor\Mailbox;
+
+use Innmind\Witness\{
+    Actor\Mailbox,
+    Actor\Address,
+    Message\Payload\Serialized,
+};
+use Innmind\Immutable\{
+    Maybe,
+    Sequence,
+    SideEffect,
+};
+
+final class Nil implements Mailbox
+{
+    private function __construct(
+        private Address\Name $name,
+    ) {
+    }
+
+    public static function of(Address\Name $name): self
+    {
+        return new self($name);
+    }
+
+    public function address(Address\Name $sender): Address
+    {
+        return Address::of($this->name, $this, $sender);
+    }
+
+    public function push(Sequence $messages): Maybe
+    {
+        // Act as if the messages were sent so the calling actor can't know the
+        // actor no longer exist.
+        return Maybe::just(new SideEffect);
+    }
+
+    public function pull(): Maybe
+    {
+        // This method should never be called by Process since a Nil mailbox
+        // should only be built when decoding a message where the address no
+        // longer exist.
+        /** @var Maybe<Serialized> */
+        return Maybe::nothing();
+    }
+}
